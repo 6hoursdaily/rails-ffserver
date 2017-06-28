1106e310fdf2bb2d54153f984da17b01ef5922f7
Jerry Pascua <jerrypascua@gmail.com>
2017-06-28 19:06:11 +0800

    added ffserver initialization with foreman

---
+++ boot/ffserver.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source .env
+
+cctv_baseurl=$CCTV_BASEURL
+sources=( 51 52 53 54 55 57 ) # list of IP camera hosts
+
+# start ffmpeg inputs in its own shell
+for source in "${sources[@]}"; do
+  echo "starting stream $source"
+  (
+    $FFMPEG_LOC -loglevel "error" -r 20 -i $CCTV_BASEURL$source/onvif1 -vcodec copy -an http://localhost:8090/cam$source.ffm
+  ) &
+  sleep 1
+done; wait
