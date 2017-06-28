610a85d102a510342517bc05efb924c18e017295
Jerry Pascua <jerrypascua@gmail.com>
2017-06-28 20:18:38 +0800

    changed framerate to 15

--- boot/ffserver.sh
+++ boot/ffserver.sh
@@ -8,7 +8,7 @@ sources=( 51 52 53 54 55 57 ) # list of IP camera hosts
 for source in "${sources[@]}"; do
   echo "starting stream $source"
   (
-    $FFMPEG_LOC -loglevel "error" -r 20 -i $CCTV_BASEURL$source/onvif1 -vcodec copy -an http://localhost:8090/cam$source.ffm
+    $FFMPEG_LOC -loglevel "error" -r 15 -i $CCTV_BASEURL$source/onvif1 -vcodec copy -an http://localhost:8090/cam$source.ffm
   ) &
   sleep 1
 done; wait
